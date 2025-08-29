629444056e6c9f796e4d6695dca7fc38382c8873
Victor Wu <victorlwu0512@gmail.com>
2025-08-28 21:32:28 -0600

    Fix errors

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // The public directory will be copied to the root of the dist folder
+
   publicDir: 'public',
   build: {
     outDir: 'dist',
-    // Optional: minify for smaller file size
+
     minify: 'terser',
     rollupOptions: {
       input: {
-        main: 'public/index.html' // Specify the entry point
+        main: 'index.html'
       }
     }
   }
